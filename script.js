7568b99ac24b4689cc1ecd17fe26e7017bde9aa6
KBarnes2001 <76533295+KBarnes2001@users.noreply.github.com>
2021-03-01 21:31:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+<link href="algolia.css" rel="stylesheet" type="text/css" />
+ const searchClient = algoliasearch('O22RB5L6OV', 'df701a886b2fc9905f6f34d4d3f321d2');
+
+const search = instantsearch({
+indexName: 'Email-Index',
+searchClient,
+});
+
+search.addWidgets([
+instantsearch.widgets.searchBox({
+container: '#searchbox',
+}),
+
+instantsearch.widgets.configure({
+hitsPerPage: 16
+}),
+
+instantsearch.widgets.hits({
+container: '#hits',
+templates: {
+item: `
+<div>
+<div class="hit-name">
+{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+</div>
+</div>
+`,
+},
+}),
+
+instantsearch.widgets.pagination({
+container: '#pagination',
+}),
+]);
+
+search.start();
